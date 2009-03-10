e84f251b3f860fc36a6a70c2678ccdc2f0d1b2d4
vasiliauskas.agnius <vasiliauskas.agnius@20134e94-fa4a-0410-bb3d-fd7e3769c28f>
2009-03-10 21:15:45 +0000

    some comments added

--- trunk/Python/detect_copymove.py
+++ trunk/Python/detect_copymove.py
@@ -4,11 +4,17 @@ import operator as op
 from optparse import OptionParser
 
 def Dist(p1,p2):
+	"""
+	Euclidean distance between 2 points
+	"""
     x1, y1 = p1
     x2, y2 = p2
     return (((x1-x2)*(x1-x2)) + ((y1-y2)*(y1-y2)))**0.5
 
-def intesectarea(p1,p2,size):
+def intersectarea(p1,p2,size):
+	"""
+	Given 2 boxes, this function returns intersection area
+	"""
 	x1, y1 = p1
 	x2, y2 = p2
 	ix1, iy1 = max(x1,x2), max(y1,y2)
@@ -18,6 +24,11 @@ def intesectarea(p1,p2,size):
 	return iarea
 
 def Hausdorff_distance(clust1, clust2, forward, dir):
+	"""
+	Function measures distance between 2 sets. (Some kind of similarity between 2 sets if you like).
+	It is modified Hausdorff distance, because instead of max distance - average distance is taken.
+	This is done for function being more error-prone to cluster coordinates.
+	"""
     if forward == None:
         return max(Hausdorff_distance(clust1,clust2,True,dir),Hausdorff_distance(clust1,clust2,False,dir))
     else:
@@ -26,6 +37,9 @@ def Hausdorff_distance(clust1, clust2, forward, dir):
         return sum([min([Dist((p1[0]+dx,p1[1]+dy),p2) for p2 in clend]) for p1 in clstart])/len(clstart)
 
 def hassimilarcluster(ind, clusters):
+	"""
+	For given cluster tells does it have twin cluster in image or not.
+	"""
 	item = op.itemgetter
 	global opt
 	found = False
@@ -37,37 +51,49 @@ def hassimilarcluster(ind, clusters):
 			cy = min(cl,key=item(1))[1]
 			dx, dy = cx - tx, cy - ty
 			specdist = Hausdorff_distance(clusters[ind],cl,None,(dx,dy))
-			if specdist <= int(opt.clsim):
+			if specdist <= int(opt.rgsim):
 				found = True
 				break
 	return found
 
 def blockpoints(pix, coords, size):
+	"""
+	Generator of pixel colors of given block.
+	"""
     xs, ys = coords
     for x in range(xs,xs+size):
         for y in range(ys,ys+size):
             yield pix[x,y]
 
 def colortopalette(color, palette):
+	"""
+	Convert given color into palette color.
+	"""
 	for a,b in palette:
 		if color >= a and color < b:
 			return b
 
 def imagetopalette(image, palcolors):
+	"""
+	Convert given image into custom palette colors
+	"""
 	assert image.mode == 'L', "Only grayscale images supported !"
 	pal = [(palcolors[i],palcolors[i+1]) for i in range(len(palcolors)-1)]
 	image.putdata([colortopalette(c,pal) for c in list(image.getdata())])
 
 def getparts(image, block_len):
+ """
+ Decompose given image into small blocks of data.
+ """
  img = image.convert('L') if image.mode != 'L' else image
  w, h = img.size 
  parts = []
  # Bluring image for abandoning image details and noise.
  global opt
- for n in range(int(opt.blevel)):
+ for n in range(int(opt.imblev)):
   img = img.filter(ImageFilter.SMOOTH_MORE)
  # Converting image to custom palette
- imagetopalette(img, [x for x in range(256) if x%15 == 0])
+ imagetopalette(img, [x for x in range(256) if x%int(opt.impalred) == 0])
  pix = img.load()
  
  for x in range(w-block_len):
@@ -78,20 +104,19 @@ def getparts(image, block_len):
  return parts
 
 def similarparts(imagparts):
+ """
+ Return only these blocks which are similar by content.
+ """
  dupl = []
- th = 200
  global opt
  l = len(imagparts[0])-1
  
  for i in range(len(imagparts)-1):  
-  difs=0
-  for x,y in zip(imagparts[i][:l],imagparts[i+1][:l]):
-   difs += abs(x-y)
-  
+  difs = sum(abs(x-y) for x,y in zip(imagparts[i][:l],imagparts[i+1][:l]))
   mean = float(sum(imagparts[i][:l])) / l
   dev = float(sum(abs(mean-val) for val in imagparts[i][:l])) / l
-  if dev/mean >= float(opt.coldev):
-    if difs <= th:
+  if dev/mean >= float(opt.blcoldev):
+    if difs <= int(opt.blsim):
       if imagparts[i] not in dupl:
        dupl.append(imagparts[i])
       if imagparts[i+1] not in dupl:
@@ -101,14 +126,13 @@ def similarparts(imagparts):
 
 def clusterparts(parts, block_len):
  """
- Grouping nearest images into groups.
- This is done, because we need to
- filter out very small groups. We
- want to know only big differences.
+ Further filtering out non essential blocks.
+ This is done by clustering blocks at first and after that
+ filtering out small clusters and clusters which doesn`t have
+ twin cluster in image.
  """
  parts = sorted(parts, key=op.itemgetter(-1))
- ath = 0.02
- sth = 1.2
+ global opt
  clusters = [[parts[0][-1]]]
  
  # assign all parts to clusters
@@ -119,9 +143,9 @@ def clusterparts(parts, block_len):
   fc = []
   for k,cl in enumerate(clusters):
    for xc,yc in cl:
-    ar = intesectarea((xc,yc),(x,y),block_len)
+    ar = intersectarea((xc,yc),(x,y),block_len)
     intrat = float(ar)/(block_len*block_len)
-    if intrat > ath:
+    if intrat > float(opt.blint):
    	 if not fc: clusters[k].append((x,y))
    	 fc.append(k)
    	 break
@@ -138,7 +162,7 @@ def clusterparts(parts, block_len):
  
  item = op.itemgetter
  # filter out small clusters
- clusters = [clust for clust in clusters if Dist((min(clust,key=item(0))[0],min(clust,key=item(1))[1]), (max(clust,key=item(0))[0],max(clust,key=item(1))[1]))/(block_len*1.4) >= sth]
+ clusters = [clust for clust in clusters if Dist((min(clust,key=item(0))[0],min(clust,key=item(1))[1]), (max(clust,key=item(0))[0],max(clust,key=item(1))[1]))/(block_len*1.4) >= float(opt.rgsize)]
  
  # filter out clusters, which doesn`t have identical twin cluster
  clusters = [clust for x,clust in enumerate(clusters) if hassimilarcluster(x,clusters)]
@@ -147,29 +171,33 @@ def clusterparts(parts, block_len):
 
 def marksimilar(image, clust, size):
  """
- Mark found similar image blocks on
- original image, by applying red layer
- on similar parts of image.
+ Draw discovered similar image regions.
  """
+ global opt
  if clust:
   draw = ImageDraw.Draw(image)
   for cl in clust:
    for x,y in cl:
     draw.rectangle((x,y,x+size,y+size),fill="red")
-  for cl in clust:
-   cx1 = min([cx for cx,cy in cl])
-   cy1 = min([cy for cx,cy in cl])
-   cx2 = max([cx for cx,cy in cl]) + block_len
-   cy2 = max([cy for cx,cy in cl]) + block_len
-   draw.rectangle([cx1,cy1,cx2,cy2],outline="blue")
+  if int(opt.imauto):
+   for cl in clust:
+    cx1 = min([cx for cx,cy in cl])
+    cy1 = min([cy for cx,cy in cl])
+    cx2 = max([cx for cx,cy in cl]) + block_len
+    cy2 = max([cy for cx,cy in cl]) + block_len
+    draw.rectangle([cx1,cy1,cx2,cy2],outline="blue")
  return image
 
 if __name__ == '__main__':
  cmd = OptionParser("usage: %prog image_file [options]")
- cmd.add_option('', '--debug', help='Do not search identical regions. (default: %default)', default=0)
- cmd.add_option('', '--blevel',help='Blur level for degrading image details. (default: %default)', default=8)
- cmd.add_option('', '--clsim', help='Region similarity threshold. (default: %default)', default=5)
- cmd.add_option('', '--coldev', help='Block color deviation threshold. (default: %default)', default=0.2)
+ cmd.add_option('', '--imauto', help='Automatically search identical regions. (default: %default)', default=1)
+ cmd.add_option('', '--imblev',help='Blur level for degrading image details. (default: %default)', default=8)
+ cmd.add_option('', '--impalred',help='Image palette reduction factor. (default: %default)', default=15)
+ cmd.add_option('', '--rgsim', help='Region similarity threshold. (default: %default)', default=5)
+ cmd.add_option('', '--rgsize',help='Region size threshold. (default: %default)', default=1.2)
+ cmd.add_option('', '--blsim', help='Block similarity threshold. (default: %default)',default=200)
+ cmd.add_option('', '--blcoldev', help='Block color deviation threshold. (default: %default)', default=0.2)
+ cmd.add_option('', '--blint', help='Block intersection threshold. (default: %default)', default=0.02)
  opt, args = cmd.parse_args()
  if not args:
   cmd.print_help()
@@ -179,9 +207,9 @@ if __name__ == '__main__':
  im = Image.open(args[0])
  lparts = getparts(im, block_len)
  dparts = similarparts(lparts)
- cparts = clusterparts(dparts, block_len) if not int(opt.debug) else [[elem[-1] for elem in dparts]]
+ cparts = clusterparts(dparts, block_len) if int(opt.imauto) else [[elem[-1] for elem in dparts]]
  im = marksimilar(im, cparts, block_len)
  out = args[0].split('.')[0] + '_analyzed.jpg'
  im.save(out)
- print 'Done. Found', len(cparts) if not int(opt.debug) else 0, 'identical regions'
+ print 'Done. Found', len(cparts) if int(opt.imauto) else 0, 'identical regions'
  print 'Output is saved in file -', out
