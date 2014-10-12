5f249e7e10cda4c02aea22d735d1acc4b0a01b31
vasiliauskas.agnius <vasiliauskas.agnius@gmail.com>
2014-10-12 21:44:05 +0000

    Removed fixed number of front lines creation. Now such number of front lines will be created as it fits into time out given by process. This gives around 30% win probability improvement compared to old version of bot with fixed number of front lines.

--- warLightAI/src/main/Region.java
+++ warLightAI/src/main/Region.java
@@ -21,7 +21,6 @@ public class Region {
 	private SuperRegion superRegion;
 	private int armies;
 	private String playerName;
-        public ArrayList<Region> lastAttackPath; // for testing purposes only
         public ArrayList<Region> lastAttackPath1;
         public ArrayList<Region> lastAttackPath2;
         
